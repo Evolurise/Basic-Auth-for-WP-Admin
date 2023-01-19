330e967bcd0e0cfb36ab30d6dc0808cd70c5d6d2
Evolurise : Web Agency <122077963+Evolurise@users.noreply.github.com>
2023-01-19 23:22:29 +0100

    Refractor including data sanitization and escaping

--- wp-admin-basic-auth.php
+++ wp-admin-basic-auth.php
@@ -27,11 +27,16 @@ License: GPL2
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+add_action( 'init', 'basic_auth_for_wp_admin' );
+add_action( 'admin_init', 'basic_auth_for_wp_admin_options_init' );
+echo '<link rel="stylesheet" type="text/css" href="' . esc_url( plugin_dir_url( __FILE__ ) . 'styles_admin.css' ) . '">';
+
+
 function basic_auth_for_wp_admin() {
     if ( ! is_user_logged_in() ) {
         $options = get_option( 'basic_auth_for_wp_admin_options' );
-        $user = $options['username'];
-        $pass = $options['password'];
+        $user = esc_attr( $options['username'] );
+        $pass = esc_attr( $options['password'] );
         $valid_pages = array( '/wp-admin/', '/wp-login.php' );
         $valid = false;
         foreach ( $valid_pages as $page ) {
@@ -46,73 +51,69 @@ function basic_auth_for_wp_admin() {
         if ( ! isset( $_SERVER['PHP_AUTH_USER'] ) || ! isset( $_SERVER['PHP_AUTH_PW'] ) ) {
             header( 'WWW-Authenticate: Basic realm="Access denied"' );
             header( 'HTTP/1.0 401 Unauthorized' );
-            die( 'Access denied' );
+            die( esc_html( 'Access denied' ) );
         }
-        if ( $_SERVER['PHP_AUTH_USER'] != $user || $_SERVER['PHP_AUTH_PW'] != $pass ) {
+        if ( $_SERVER['PHP_AUTH_USER'] != $user || !wp_check_password($_SERVER['PHP_AUTH_PW'],$pass,$user) ) {
             header( 'WWW-Authenticate: Basic realm="Access denied"' );
             header( 'HTTP/1.0 401 Unauthorized' );
-            die( 'Access denied' );
+            die( esc_html( 'Access denied' ) );
         }
     }
 }
-add_action( 'init', 'basic_auth_for_wp_admin' );
 
 function basic_auth_for_wp_admin_options_init() {
     register_setting( 'basic_auth_for_wp_admin_options', 'basic_auth_for_wp_admin_options', 'basic_auth_for_wp_admin_options_validate' );
-    add_settings_section( 'basic_auth_for_wp_admin_section', 'Basic Auth for WP-Admin Settings', 'basic_auth_for_wp_admin_section_callback', 'basic_auth_for_wp_admin' );
-    add_settings_field( 'basic_auth_for_wp_admin_username', 'Username', 'basic_auth_for_wp_admin_username_callback', 'basic_auth_for_wp_admin', 'basic_auth_for_wp_admin_section' );
-    add_settings_field( 'basic_auth_for_wp_admin_password', 'Password', 'basic_auth_for_wp_admin_password_callback', 'basic_auth_for_wp_admin', 'basic_auth_for_wp_admin_section' );
+    add_settings_section( 'basic_auth_for_wp_admin_section', esc_html__( 'Basic Auth for WP-Admin Settings', 'text-domain' ), 'basic_auth_for_wp_admin_section_callback', 'basic_auth_for_wp_admin' );
+    add_settings_field( 'basic_auth_for_wp_admin_username', esc_html__( 'Username', 'text-domain' ), 'basic_auth_for_wp_admin_username_callback', 'basic_auth_for_wp_admin', 'basic_auth_for_wp_admin_section' );
+    add_settings_field( 'basic_auth_for_wp_admin_password', esc_html__( 'Password', 'text-domain' ), 'basic_auth_for_wp_admin_password_callback', 'basic_auth_for_wp_admin', 'basic_auth_for_wp_admin_section' );
 }
-add_action( 'admin_init', 'basic_auth_for_wp_admin_options_init' );
 
 function basic_auth_for_wp_admin_section_callback() {
-    echo '<p>Enter a username and password to use for basic authentication on the wp-admin and wp-login pages.</p>';
+    echo '<p>' . esc_html__( 'Enter a username and password to use for basic authentication on the wp-admin and wp-login pages.', 'text-domain' ) . '</p>';
 }
 
 function basic_auth_for_wp_admin_username_callback() {
     $options = get_option( 'basic_auth_for_wp_admin_options' );
-    echo '<input id="basic_auth_for_wp_admin_username" name="basic_auth_for_wp_admin_options[username]" size="40" type="text" value="' . $options['username'] . '" />';
+    echo '<input id="basic_auth_for_wp_admin_username" name="basic_auth_for_wp_admin_options[username]" size="40" type="text" value="' . esc_attr( $options['username'] ) . '" />';
 }
 
 function basic_auth_for_wp_admin_password_callback() {
     $options = get_option( 'basic_auth_for_wp_admin_options' );
-    echo '<input id="basic_auth_for_wp_admin_password" name="basic_auth_for_wp_admin_options[password]" size="40" type="password" value="' . $options['password'] . '" />';
-    echo '<input type="checkbox" id="basic_auth_for_wp_admin_show_password" name="basic_auth_for_wp_admin_options[show_password]"' . checked( $options['show_password'], 1, false ) . ' value="1"> Show Password</input>';
-    wp_enqueue_script( 'basic_auth_for_wp_admin_options', plugin_dir_url( __FILE__ ) . 'password_checker.js', array(), '1.0.0', true );
-
-    ?>
-   
-<?php
+    echo '<input id="basic_auth_for_wp_admin_password" name="basic_auth_for_wp_admin_options[password]" size="40" type="password" value="' . esc_attr( $options['password'] ) . '" />';
 }
 
 function basic_auth_for_wp_admin_options_validate( $input ) {
+    $input['username'] = sanitize_text_field( $input['username'] );
     $options = get_option( 'basic_auth_for_wp_admin_options' );
-    $options['username'] = trim( $input['username'] );
-    $options['password'] = trim( $input['password'] );
-    $input['show_password'] = sanitize_text_field( $input['show_password'] );
-    return $options;
+    if ($input['password'] != $options['password']) {
+        $input['password'] = password_hash($input['password'], PASSWORD_DEFAULT);
+    }
+    return $input;
+}
+function basic_auth_for_wp_admin_options_page() {
+    if ( ! current_user_can( 'manage_options' ) ) {
+        wp_die( esc_html__( 'You do not have sufficient permissions to access this page.', 'text-domain' ) );
+    }
+    ?>
+    <div class="wrap">
+        <img width="20%" src="<?php echo esc_url( plugin_dir_url( __FILE__ ) . '/img/evolurise_logo.png' ); ?>" alt="Evolurise logo">
+        <h2><?php echo esc_html__( 'Welcome to the Basic Auth for WP-Admin settings page', 'text-domain' ); ?></h2>
+        <form action="options.php" method="post">
+            <?php
+            settings_fields( 'basic_auth_for_wp_admin_options' );
+            do_settings_sections( 'basic_auth_for_wp_admin' );
+            submit_button();
+            ?>
+        </form>
+        <p>Thank you for using our plugin, please rate it and visit our website <a href="https://www.evolurise.com">evolurise.com</a></p>
+    </div>
+    <?php
 }
 
 function basic_auth_for_wp_admin_menu() {
-    add_options_page( 'Basic Auth for WP-Admin Options', 'Basic Auth for WP-Admin', 'manage_options', 'basic_auth_for_wp_admin', 'basic_auth_for_wp_admin_options_page' );
+    add_options_page( esc_html__( 'Basic Auth for WP-Admin', 'text-domain' ), esc_html__( 'Basic Auth for WP-Admin', 'text-domain' ), 'manage_options', 'basic_auth_for_wp_admin', 'basic_auth_for_wp_admin_options_page' );
 }
+
 add_action( 'admin_menu', 'basic_auth_for_wp_admin_menu' );
 
-function basic_auth_for_wp_admin_options_page() {
-    wp_enqueue_style( 'basic-auth-for-wp-admin-style', plugin_dir_url( __FILE__ ) . 'styles_admin.css' );
-    if ( ! current_user_can( 'manage_options' ) ) {
-        wp_die( 'You do not have sufficient permissions to access this page.' );
-    }
-
-    echo '<div class="wrap">';
-    echo '<img width="20%" src="' . plugin_dir_url( __FILE__ ) . '/img/evolurise_logo.png' . '"</img>';
-    echo '<h2>Welcome to the Basic Auth for WP-Admin settings page</h2>';
-    echo '<form action="options.php" method="post">';
-    settings_fields( 'basic_auth_for_wp_admin_options' );
-    do_settings_sections( 'basic_auth_for_wp_admin' );
-    submit_button();
-    echo 'Thank you for using our plugin, please rate it and visit our website <a href="https://www.evolurise.com">evolurise.com</a>';
-    echo '</form>';
-    echo '</div>';
-}
 
