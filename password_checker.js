c48d54e84f57e7bf2effa572cbfc45a03ed192c5
Evolurise : Web Agency <122077963+Evolurise@users.noreply.github.com>
2023-01-21 22:48:32 +0100

    Delete password_checker.js

--- password_checker.js
+++
@@ -1,6 +0,0 @@
-var checkbox = document.getElementById("basic_auth_for_wp_admin_show_password");
-var passwordInput = document.getElementById("basic_auth_for_wp_admin_password");
-
-checkbox.addEventListener("change", function() {
-    passwordInput.type = this.checked ? "text" : "password";
-});
